d13bd074286c228ceafb50f2f4bf1179f771ff4a
Magomed Makhmudov <mmriadovich@gmail.com>
2020-07-01 00:11:00 +0300

    feat: login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { InjectEntityManager } from '@nestjs/typeorm';
 import { EntityManager } from 'typeorm';
+import {User} from '../entity/User';
+
+const jwt = require('jsonwebtoken');
+const uuid = require('uuid/v4');
 
 @Injectable()
 export class AuthService {
     constructor(
-        @InjectEntityManager() manager: EntityManager,
+        @InjectEntityManager() private readonly manager: EntityManager,
     ) { }
 
-    login(body) {
+    async login(body) {
+        const user = await this.manager.getRepository(User).findOne({where:{username: body.login}})
+        if (user !== null){
+            if (user.password !== body.password){
+                throw new Error('Invalid password')
+            }
+            const refresh_token = uuid()
+            const access_token = jwt.sign({id: user.id}, 'mind')
+            return {
+                refresh_token,
+                access_token
+            }
+        }
         // TODO
     }
 }
