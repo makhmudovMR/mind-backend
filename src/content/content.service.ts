b9c75363fefeb6694bacdac5e612592a7050368c
Magomed Makhmudov <mmriadovich@gmail.com>
2020-07-17 16:56:39 +0300

    fix: some fix

--- src/content/content.service.ts
+++ src/content/content.service.ts
@@ -13,19 +13,19 @@ export class ContentService {
 
     async getUser(req, body) {
         Logger.log(req.userInfo);
-        const user = await this.manager.getRepository(User).findOne({where: {id: req.userInfo.userId}});
+        const user = await this.manager.getRepository(User).findOne({ where: { id: req.userInfo.userId } });
         Logger.log(user);
-        return {user};
+        return { user };
     }
 
-    async getUserInfo(req, body){
-        const user = await this.manager.getRepository(User).findOne({where: {id: body.userId}});
-        return {user};
+    async getUserInfo(req, body) {
+        const user = await this.manager.getRepository(User).findOne({ where: { id: body.userId } });
+        return { user };
     }
 
-    async getUserMinds(req, body){
+    async getUserMinds(req, body) {
         const user = await this.manager.getRepository(User).findOne(body.userId);
-        const minds = await this.manager.getRepository(Mind).find({where: {user: user}})
+        const minds = await this.manager.getRepository(Mind).find({ where: { user: user } })
         return minds;
     }
 
@@ -33,27 +33,27 @@ export class ContentService {
         return await this.manager.getRepository(Post).find();
     }
 
-    async getTags(){
+    async getTags() {
         return await this.manager.getRepository(MindTags).find();
     }
 
-    async followToUser(req, body){
+    async followToUser(req, body) {
         const followRelation = new FollowRelation();
         followRelation.userId = body.followingId;
         followRelation.followerId = req.userInfo.id;
         await this.manager.save(followRelation);
-        return {message: 'user is followed'};
+        return { message: 'user is followed' };
     }
 
     async getFollowingPost(req, body) {
 
-        const following = await this.manager.getRepository(FollowRelation).find({where: {followerId:req.userInfo.id}});
-        console.log(following)
+        const following = await this.manager.getRepository(FollowRelation).find({ where: { followerId: req.userInfo.id } });
+        console.log(following);
         const followingIds = following.map(item => {
             return item.userId
         })
         console.log(followingIds);
-        const minds = await this.manager.getRepository(Mind).find({where: {__user: {id: followingIds }}});
+        const minds = await this.manager.getRepository(Mind).find({ where: { __user: { id: followingIds } } });
         return minds;
     }
 
@@ -70,13 +70,13 @@ export class ContentService {
     //     return minds;
     // }
 
-    async postMind(req, body){
+    async postMind(req, body) {
         const user = await this.manager.getRepository(User).findOne(req.userInfo.id);
         const mind = new Mind();
         mind.body = "user1";
         mind.user = user;
         mind.createdDate = new Date();
         this.manager.save(mind);
-        return {message: "mind was added"};
+        return { message: "mind was added" };
     }
 }
