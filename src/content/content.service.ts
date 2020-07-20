f74fb30f94df35ee4fdc91998de169238600d40c
Magomed Makhmudov <mmriadovich@gmail.com>
2020-07-20 17:51:08 +0300

    feat: panel

--- src/content/content.service.ts
+++ src/content/content.service.ts
@@ -24,22 +24,25 @@ export class ContentService {
         return user;
     }
 
-    async getAuthUserInfo(req, body){
+    async getAuthUserInfo(req, body) {
         const user: any = await this.manager.getRepository(User).findOne({ where: { id: req.userInfo.userId } });
-        let following = await this.manager.getRepository(FollowRelation).find({where: {followerId: user.id}})
-        let followers = await this.manager.getRepository(FollowRelation).find({where: {userId: user.id}})
-        console.log(following)
-        console.log(followers)
+        let following = await this.manager.getRepository(FollowRelation).find({ where: { followerId: user.id } })
+        let followers = await this.manager.getRepository(FollowRelation).find({ where: { userId: user.id } })
+
         let followingIds = following.map(item => item.userId)
         let followersIds = followers.map(item => item.followerId);
-        console.log(followersIds);
-        console.log(followingIds);
-        let followingUsers = await this.manager.createQueryBuilder(User, 'user').where('user.id IN (:...ids)', {ids: followingIds}).getMany();
-        let followerUsers = await this.manager.createQueryBuilder(User, 'user').where('user.id IN (:...ids)', {ids: followersIds}).getMany();
+
+        let followingUsers = await this.manager.createQueryBuilder(User, 'user').where('user.id IN (:...ids)', { ids: followingIds }).getMany();
+        let followerUsers = await this.manager.createQueryBuilder(User, 'user').where('user.id IN (:...ids)', { ids: followersIds }).getMany();
+
         user.follower = followerUsers;
         user.following = followingUsers;
-        console.log(user)
-        return user
+        user.followerLength = followerUsers.length;
+        user.followingLength = followingUsers.length;
+
+        console.log(user);
+
+        return user;
     }
 
     async getUserMinds(req, body) {
@@ -67,14 +70,14 @@ export class ContentService {
     async getFollowingPost(req, body) {
         console.log(req.userInfo)
         const following = await this.manager.getRepository(FollowRelation).find({ where: { followerId: req.userInfo.userId } });
-        console.log('following',following);
+        console.log('following', following);
         const followingIds = following.map(item => {
             return item.userId
         })
-        console.log('followingIds',followingIds);
+        console.log('followingIds', followingIds);
         // const minds = await this.manager.getRepository(Mind).find({ where: { __user: { id: followingIds } } });
-        const minds = await this.manager.createQueryBuilder(Mind, 'mind').leftJoinAndSelect('mind.user', 'user').where('mind.userId IN (:...users)', {users: followingIds}).getMany();
-        console.log('minds',minds);
+        const minds = await this.manager.createQueryBuilder(Mind, 'mind').leftJoinAndSelect('mind.user', 'user').where('mind.userId IN (:...users)', { users: followingIds }).getMany();
+        console.log('minds', minds);
         return minds;
     }
 
